e6aa39577e77f88079458083fea6e1772e662e7d
wyatt88 <wyatthaha@126.com>
2015-04-23 00:02:43 +0800

    0418homework

---
+++ 0411-1-1.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+__author__ = 'wenwen'
+#for name in ['wenwen','wd','pc']:
+#    print name
+
+#for num in range(1,99):
+#    print num
+
+#print range(20)
+#for n in range(20):
+#    print n
+
+#for num in range(0,10):
+#    if num==7:
+#        break
+#    print num
+
+#for num in range(0,10):
+#    if num==7:
+#        continue
+#    print num
+
+#for n in range(10):
+#    if n==7:
+#        print n
+#        continue
+
+#for n in range(10):
+#    if n==7:
+#        continue
+#        print n
+
+#init_num=0
+#init_count=0.0
+#while True:
+#    input_number=raw_input('Input number: ')
+#    if not input_number:
+#        break
+#    else:
+#        init_count=init_count+1
+#        init_num=int(input_number)+init_num
+#print init_num/init_count
+
+#bj = 10000
+#gz=50000
+#y = 0
+#while bj < 20000:
+#    bj = bj * ( 1 + 0.0325 )
+#    print bj
+#    y = y + 1
+#print y
+
+#arrs=['C','js','python','js','css','js','html','node']
+#count=0
+#for arr in arrs:
+#    if arr=='js':
+#        count=count+1
+#print count
+#print 'js' in arrs
+
+#person = {'name':'wenwen','score':'60'}
+#print person
+#print person['name']
+#print person['score']
+#if 'name' in person:
+#    print person['name']
+#if 'wenwen' in person:
+#    print 'hello'
+#for p in person:
+#    print p
+#    print person[p]
+
+#arr=[1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
+#maxn=0
+#for num in arr:
+#    if num>maxn:
+#        maxn=num
+#print maxn
+
+#while True:
+#    year=raw_input('please input year: ')
+#    iyear=int(year)
+#    if iyear%100==0:
+#        if iyear%400==0:
+#            print year+' shi run nian'
+#            break
+#        else:
+#            continue
+#    elif iyear%4==0:
+#        print year+' shi run nian'
+#        break
+#    else:
+#        continue
+#print 'End'
+
+#person = {
+#    'name':'pc'
+#}
+#person['name'] = 'wd'
+#person['age'] = 65
+#print person
+#for p in person:
+#    print 'key is %s and value is %s' % (p,person[p])
+
+###important###
+#d = {}
+#l = ['C','js','python','js','css','js','html','node','js','python','js','css','js','html','node','js','python','js','css','js','html','node','css','js','html','node','js','python','js','css','js','html','node','js','python','js','css','js','html','node']
+#for item in l:
+#    count = 0
+#    for tmp in l:
+#        if item==tmp:
+#            count = count+1
+#    d[item]=count
+#for lang in d:
+#    print 'language is %s and count is %s' % (lang,d[lang])
+
+
+#d={}
+#l = ['C','js','python','js','css','js','html','node','js','python','js','css','js','html','node','js','python','js','css','js','html','node','css','js','html','node','js','python','js','css','js','html','node','js','python','js','css','js','html','node']
+#for item in l:
+#    if item in d:
+#        d[item]=d[item]+1
+#    else:
+#        d[item]=1
+#for lang in d:
+#    print 'language is %s and count is %s' % (lang,d[lang])
+
+#homework
+# nb way
+arr=[65555,1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,45,33,45]
+max_num=arr[0]
+sec_num=0
+for i in range(1,10):
+    for num in arr:
+        if num>max_num:
+            sec_num=max_num
+            max_num=num
+        elif max_num>num and num>sec_num:
+            sec_num=num
+print 'max='+str(max_num)
+print 'sec='+str(sec_num)
+# my way
+#arr=[1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,5555,45]
+#max_num=arr[0]
+#sec_num=0
+#for num in arr:
+#    if num>max_num:
+#        max_num=num
+#for num1 in arr:
+#        if num1>sec_num and num1!=max_num:
+#            sec_num=num1
+#print 'max='+str(max_num)
+#print 'sec='+str(sec_num)
--- hw0418.py
+++ hw0418.py
@@ -34,10 +34,11 @@ arr=[1,2,3,4,2,12,3,14,3,21,2,2,3,4111,22,3333,4]
 #        init_min=num
 #print init_min
 
-###The realization of append function
-#init_count=len(demo_list)
-#demo_list[init_count:]=['x']
-#print demo_list
+# ##The realization of append function
+# init_count=len(demo_list)
+# add_item=[2,3]
+# demo_list[init_count:]=[add_item]
+# print demo_list
 
 ###sort
 # count=0
@@ -49,5 +50,84 @@ arr=[1,2,3,4,2,12,3,14,3,21,2,2,3,4111,22,3333,4]
 #     count=count+1
 # print arr
 
+# ##The realization of extend function
+# demo_list1=[4,5,6]
+# demo_list[len(demo_list):]=demo_list1
+# print demo_list
+
+###the index of second 4
+arr_list=[1,2,3,4,2,12,3,14,3,21,2,2,3,4111,22,3333,4]
+#way 1
+# count=0
+# init_index=0
+# for item in arr_list:
+#     if item == 4:
+#         count=count+1
+#         if count==2:
+#             break
+#     init_index=init_index+1
+# print init_index
+
+#way 2
+# init_index=arr_list.index(4)
+# print  arr_list[init_index+1:].index(4)+init_index+1
+
+#way 3
+# print arr_list.index(4,arr_list.index(4)+1)
+
+###list
+# init_dothing=[]
+# init_status=True
+#
+# while init_status:
+#     action=raw_input('Input the action: ')
+#     if action=='add':
+#         thing=raw_input('Input the plan: ')
+#         init_dothing.append(thing)
+#         print init_dothing
+#     elif action=='do':
+#         if len(init_dothing)>0:
+#             print init_dothing.pop(0)
+#         else:
+#             init_status=False
+#     else:
+#         print 'You must input add or do'
+
+###The realization of reverse function
+##demo_list.reverse()
+##print demo_list
+
+# for i in range(0,len(demo_list)-1):
+#     item=demo_list[len(demo_list)-1]
+#     demo_list.remove(item)
+#     demo_list.insert(i,item)
+# #    print demo_list
+# print demo_list
+
+###
+# for item in arr_list:
+#     item_count=arr_list.count(item)
+#     if item_count>1:
+#         print(item)
+#         for i in range(1,item_count):
+#             arr_list.remove(item)
+# print arr_list
+
+###
+#way 1
+arr_list1=[1,2,3,4,2,12,3,14,3,2,12,3,14,3,21,2,2,3,4111,22,3333,4]
+arr_list2=[2,1,3,2,43,234,454,452,234,14,21,14]
+# d={}
+# for num in arr_list1:
+#     if num in arr_list2:
+#         d[num]=arr_list1.count(num)+arr_list2.count(num)
+# print d
+#way 2
+# for i in arr_list1:
+#     for j in arr_list2:
+#         if i==j:
+#             print i
 
 
+
+###HomeWork####
