0026a0f710a2554608202ad815aeafe226d6c543
wyatt88 <wyatthaha@126.com>
2015-05-09 11:33:19 +0800

    20150509

--- 0509.py
+++ 0509.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-#coding=UTF-8
-
+#!/usr/bin/env python
 
 read_me = '''
 first of all, i want make it clear that i can not claim understanding this holy book  in just a few weeks, and i would not dare comment on this sacred book, in addition, i don't think i can give you a full picture of the holy bible in just few words.i can not preach anything here. what i want to do here is to express my understanding by sharing two events described in this book. the fist story i want to share is abandoned tower of babel.according to the bibel,people use the same language to communicate with each other in ancient times.with the soaring vanity,they decided to build a heaven-reaching tower to show off their achievement, god knows, he change the human language into different kinds and make it difficult for people to communicate with each other,hence the failure of building tower of  babel.this story tells people,never do things in selfishness, but make a life out of enternal glory.the other story,before jesus christ was crucified,he said, father,forgive them, for they know not what they do. with great love, he shouldered all the sins of  people. what can we learn from this story?we live in this world thanks to the love of god, for this reanson, we should make our lives glorious to honor our god.finally,i want to sum up by saying that only if we put our lives in the eternal love of god,can we live a perfect life, and  what you appealed is what god expected!
@@ -15,22 +13,20 @@ for i in range(0,len(temp_list)):
     for j in range(0,len(temp_list)-1):
         if temp_list[j][1]>temp_list[j+1][1]:
             temp_list[j],temp_list[j+1]=temp_list[j+1],temp_list[j]
+f=open('demo.txt','a')
 for index in range(1,11):
-    print(temp_list[-index])
-###way 1
-# count_list=str_dict.values()
-# count_list.sort(reverse=True)
-# for k,v in str_dict.items():
-#     for i in range(0,10):
-#         if count_list[i]==v:
-#             new_dict[k]=v
-###way2
-temp_list=str_dict.items()
-for i in range(0,len(temp_list)):
-    for j in range(0,len(temp_list)-1):
-        if temp_list[j][1]>temp_list[j+1][1]:
-            temp_list[j],temp_list[j+1]=temp_list[j+1],temp_list[j]
-#        print temp_list
-#print temp_list
-for index in range(1,11):
-    print(temp_list[-index])
+    string='No.'+str(index)+' is '+temp_list[-index][0]+' , and count is '+str(temp_list[-index][1])+'\n'
+    f.write(string)
+f.close()
+
+# f = open('www_access_20140823.log')
+#file_str = str(f.read())
+# str_list=file_str.split(' ')
+# ip_list=[]
+# ip_list.append(str_list[0])
+# for ip in str_list:
+#     if ip == '"-"':
+#         ip_list.append(str_list.append(str_list.index(ip)+1))
+
+
+
