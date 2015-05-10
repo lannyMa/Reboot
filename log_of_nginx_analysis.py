83637c6fa0a270434c7235338901f1d2e11dd64a
wyatt88 <wyatthaha@126.com>
2015-05-11 00:52:09 +0800

    homework0509

--- log_of_nginx_analysis.py
+++ log_of_nginx_analysis.py
@@ -5,16 +5,20 @@ log_file=open('www_access_20140823.log','r')
 log_list=[]
 log_dict={}
 for line in log_file.readlines():
-    log_list=[]
     tmp_list=line.split()
-    log_list.append(tmp_list[8])
-    log_list.append(tmp_list[6])
+    tmp_tuple=(tmp_list[8],tmp_list[6],tmp_list[0])
+    # log_list.append(tmp_list[6])
     # log_list.append(tmp_list[0])
-    if tmp_list[0] not in log_dict:
-        log_dict[tmp_list[0]]=1
+    if tmp_tuple not in log_dict:
+        log_dict[tmp_tuple]=1
     else:
-        log_dict[tmp_list[0]]=log_dict[tmp_list[0]]+1
-    log_list.append((tmp_list[0],log_dict[tmp_list[0]]))
+        log_dict[tmp_tuple]=log_dict[tmp_tuple]+1
     # print log_dict[tmp_list[0]]
+    # print log_list
+for (k,v) in log_dict.items():
+    log_list=[]
+    log_list.append(k[0])
+    log_list.append(k[1])
+    log_list.append((k[2],v))
     print log_list
 log_file.close()
