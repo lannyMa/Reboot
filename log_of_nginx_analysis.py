884a9aadaaf561b2e5ef4a3f579673fef1def070
wyatt88 <wyatthaha@126.com>
2015-05-11 00:22:28 +0800

    homework0509

--- log_of_nginx_analysis.py
+++ log_of_nginx_analysis.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
 #~*~coding:UTF-8~*~
+
+log_file=open('www_access_20140823.log','r')
+log_list=[]
+log_dict={}
+for line in log_file.readlines():
+    log_list=[]
+    tmp_list=line.split()
+    log_list.append(tmp_list[8])
+    log_list.append(tmp_list[6])
+    # log_list.append(tmp_list[0])
+    if tmp_list[0] not in log_dict:
+        log_dict[tmp_list[0]]=1
+    else:
+        log_dict[tmp_list[0]]=log_dict[tmp_list[0]]+1
+    log_list.append((tmp_list[0],log_dict[tmp_list[0]]))
+    # print log_dict[tmp_list[0]]
+    print log_list
+log_file.close()
