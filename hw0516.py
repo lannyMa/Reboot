a3df6ae32a1d5c35f810e61220b1eab924422e5a
wyatt88 <wyatthaha@126.com>
2015-05-22 16:37:29 +0800

    homework0516

---
+++ hw0516.py
@@ -0,0 +1,12 @@
+# init_list=[(1,4),(5,1),(2,3)]
+# # def sorted_obj(obj):
+# #     return max(obj)
+# def compare(obj):
+#     if obj[0]>obj[1]:
+#         return obj[0]
+#     else:
+#         return obj[1]
+# # new_list=sorted(init_list,key=sorted_obj)
+# # new_list=sorted(init_list,key=lambda obj:max(obj))
+# new_list=sorted(init_list,key=lambda obj:compare(obj))
+# print new_list
--- log_of_nginx_analysis.py
+++ log_of_nginx_analysis.py
@@ -27,41 +27,46 @@ for line in log_file.readlines():
 #     print log_list
 
 tmp_list1=log_dict.items()
-for i in range(10):
-    for j in range(len(tmp_list1)-1):
-        if tmp_list1[j][1]>tmp_list1[j+1][1]:
-            tmp_list1[j],tmp_list1[j+1]=tmp_list1[j+1],tmp_list1[j]
-print tmp_list1[-11:-1]
-ip_dict={}
-url_dict={}
-for (k,v) in tmp_list1:
-    tmp_ip=k[2]
-    tmp_url=k[1]
-    tmp_ip_dict={}
-    ip_count=0
-    if tmp_ip not in ip_dict:
-        for (key,value) in tmp_list1:
-            if tmp_ip==key[2]:
-                ip_count=ip_count+value
-        ip_dict[tmp_ip]=ip_count
-    if tmp_url not in url_dict:
-        tmp_ip_dict[tmp_ip]=v
-        url_dict[tmp_url]=tmp_ip_dict
-    else:
-        if tmp_ip not in url_dict[tmp_url]:
-            url_dict[tmp_url][tmp_ip]=v
+# for i in range(10):
+#     for j in range(len(tmp_list1)-1):
+#         if tmp_list1[j][1]>tmp_list1[j+1][1]:
+#             tmp_list1[j],tmp_list1[j+1]=tmp_list1[j+1],tmp_list1[j]
+#print tmp_list1
+def sort_count(t):
+    return t[1]
+for s in sorted(tmp_list1,key=sort_count):
+    print s
+# print tmp_list1[0:9]
+# ip_dict={}
+# url_dict={}
+# for (k,v) in tmp_list1:
+#     tmp_ip=k[2]
+#     tmp_url=k[1]
+#     tmp_ip_dict={}
+#     ip_count=0
+#     if tmp_ip not in ip_dict:
+#         for (key,value) in tmp_list1:
+#             if tmp_ip==key[2]:
+#                 ip_count=ip_count+value
+#         ip_dict[tmp_ip]=ip_count
+#     if tmp_url not in url_dict:
+#         tmp_ip_dict[tmp_ip]=v
+#         url_dict[tmp_url]=tmp_ip_dict
+#     else:
+#         if tmp_ip not in url_dict[tmp_url]:
+#             url_dict[tmp_url][tmp_ip]=v
 # print url_dict['/data/uploads/2014/0722/23/small_53ce859e37394.jpg']
 #print ip_dict
-ip_list=ip_dict.items()
-for i in range(10):
-    for j in range(len(ip_list)-1):
-        if ip_list[j][1]>ip_list[j+1][1]:
-            ip_list[j],ip_list[j+1]=ip_list[j+1],ip_list[j]
-print ip_list[-11:-1]
-for i in range(0,len(log_dict)):
-    log_list.append(tmp_list1[i][0][0])
-    log_list.append(tmp_list1[i][0][1])
-    log_list.append((tmp_list1[i][0][2],tmp_list1[i][1]))
+# ip_list=ip_dict.items()
+# for i in range(10):
+#     for j in range(len(ip_list)-1):
+#         if ip_list[j][1]>ip_list[j+1][1]:
+#             ip_list[j],ip_list[j+1]=ip_list[j+1],ip_list[j]
+# print ip_list[-11:-1]
+# for i in range(0,len(log_dict)):
+#     log_list.append(tmp_list1[i][0][0])
+#     log_list.append(tmp_list1[i][0][1])
+#     log_list.append((tmp_list1[i][0][2],tmp_list1[i][1]))
     # tmp_file=open('log_analysis1.txt','a')
 #     # # tmp_file.write(str(log_file))
 #     tmp_file.write(str(log_list)+'\n')
